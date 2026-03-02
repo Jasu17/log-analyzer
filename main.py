033d385950bfa534231a53ccd6131c6a031c29c0
Jasu17 <jaasmu.9@gmail.com>
2026-03-02 17:13:39 -0500

    feat: Implement IP-based flood detection with time window analtsis

--- main.py
+++ main.py
@@ -1,12 +1,29 @@
-from src.parser import parse_line
 from collections import defaultdict
+from datetime import timedelta
+from src.parser import parse_line
 
-ip_counter = defaultdict(int)
+WINDOW = timedelta(seconds=10)
+THRESHOLD = 10
+
+ip_timestamps = defaultdict(list)
 
 with open("/var/log/httpd/access_log") as f:
     for line in f:
         parsed = parse_line(line)
         if parsed:
-            ip_counter[parsed["ip"]] +=1
+            ip_timestamps[parsed["ip"]].append(parsed["time"])
 
-print(dict(ip_counter))
+for ip, timestamps in ip_timestamps.items():
+    timestamps.sort()
+
+    for i in range(len(timestamps)):
+        count = 1
+        for j in range(i + 1, len(timestamps)):
+            if timestamps[j] - timestamps[i] <= WINDOW:
+                count += 1
+            else:
+                break
+
+        if count >= THRESHOLD:
+            print(f"[ALERT] Possible flood from {ip}: {count} requests in {WINDOW}")
+            break
