10676444cd12609bc99408991d900e2a097a0bb6
Javier <139712961+Jasu17@users.noreply.github.com>
2026-03-04 16:59:07 -0500

    modularization of functions

--- main.py
+++ main.py
@@ -1,29 +1,7 @@
-from collections import defaultdict
-from datetime import timedelta
-from src.parser import parse_line
+from src.analyzer import analyze_log
 
-WINDOW = timedelta(seconds=10)
-THRESHOLD = 10
+def main():
+    analyze_log("/var/log/httpd/access_log")
 
-ip_timestamps = defaultdict(list)
-
-with open("/var/log/httpd/access_log") as f:
-    for line in f:
-        parsed = parse_line(line)
-        if parsed:
-            ip_timestamps[parsed["ip"]].append(parsed["time"])
-
-for ip, timestamps in ip_timestamps.items():
-    timestamps.sort()
-
-    for i in range(len(timestamps)):
-        count = 1
-        for j in range(i + 1, len(timestamps)):
-            if timestamps[j] - timestamps[i] <= WINDOW:
-                count += 1
-            else:
-                break
-
-        if count >= THRESHOLD:
-            print(f"[ALERT] Possible flood from {ip}: {count} requests in {WINDOW}")
-            break
+if __name__ == "__main__":
+    main()
---
+++ src/analyzer.py
@@ -0,0 +1,28 @@
+from collections import defaultdict
+from datetime import timedelta
+from .parser import parse_line
+
+WINDOW = timedelta(seconds=10)
+THRESHOLD = 10
+
+def analyze_log(file_path : str):
+    ip_timestamps = defaultdict(list)
+
+    with open(file_path) as f:
+        for line in f:
+            parsed = parse_line(line)
+            if parsed:
+                ip_timestamps[parsed["ip"]].append(parsed["time"])
+    detect_flood(ip_timestamps)
+
+def detect_flood(ip_timestamps):
+    for ip, timestamps in ip_timestamps.items():
+        count = 1
+        for j in range(len(timestamps)):
+            if timestamps[j] - timestamps[i] <= WINDOW:
+                count += 1
+            else:
+                break
+        if count >= THRESHOLD:
+            print(f"[ALERT] Possible flood from {ip}: {count} request")
+            break
