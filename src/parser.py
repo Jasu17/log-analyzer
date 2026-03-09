8fd698bd3b28aba0f24c8d46bde71b83eee197f6
Javier <139712961+Jasu17@users.noreply.github.com>
2026-03-09 09:45:17 -0500

    Add combined log pattern

--- src/parser.py
+++ src/parser.py
@@ -1,7 +1,7 @@
 import re
 from datetime import datetime
 
-LOG_PATTERN = re.compile(
+COMMON_LOG_PATTERN = re.compile(
     r'(?P<ip>\S+) \S+ \S+ '
     r'\[(?P<time>.*?)\] '
     r'"(?P<method>\S+) (?P<path>\S+) (?P<protocol>.*?)" '
@@ -9,18 +9,33 @@ LOG_PATTERN = re.compile(
     r'(?P<size>\S+)'
 )
 
+COMBINED_LOG_PATTERN = re.compile(
+    r'(?P<ip>\S+) \S+ \S+ '
+    r'\[(?P<time>.*?)\] '
+    r'"(?P<method>\S+) (?P<path>\S+) (?P<protocol>.*?)" '
+    r'(?P<status>\d{3}) '
+    r'(?P<size>\S+) '
+    r'"(?P<referer>.*?)" '
+    r'"(?P<user_agent>.*?)"'
+)
+
 def parse_line(line: str) -> dict | None:
-    match = LOG_PATTERN.match(line)
-    if not match:
-        return None
+    match = COMBINED_LOG_PATTERN.match(line)
+    if match:
+        data = match.groupdict()
+    else:
+        match = COMMON_LOG_PATTERN.match(line)
 
-    data = match.groupdict()
-
-    # Convertimos tipos
+        if not match:
+            return None
+        
+        data = match.groupdict()
+        data["referer"] = "-"
+        data["user_agent"] = "-"
+    
     data["status"] = int(data["status"])
     data["size"] = 0 if data["size"] == "-" else int(data["size"])
 
-    # Parseo de fecha
     data["time"] = datetime.strptime(
         data["time"],
         "%d/%b/%Y:%H:%M:%S %z"
