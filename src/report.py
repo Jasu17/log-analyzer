7baa4c452759c96923b4818483bda02352b9f15b
Javier <jaasmu.9@gmail.com>
2026-03-25 15:26:04 -0500

    Update report.py

--- src/report.py
+++ src/report.py
@@ -1,4 +1,21 @@
 from collections import Counter
+import re
+
+RISK_WEIGHTS = {
+    "flood":1,
+    "sqli":3,
+    "brute":3,
+    "scan":2,
+    "sensitive":2,
+    "user-agent":1
+}
+
+def extract_ip(alert: str):
+    match = re.search(r'from ([0-9a-fA-F\.:]+)', alert)
+    if not match:
+        return "unknown"
+    
+    return match.group(1).rstrip(":")
 
 def generate_report(events, alerts):
 
@@ -37,3 +54,48 @@ def generate_report(events, alerts):
 
     for k, v in alert_types.items():
         print(f"{k}: {v}")
+
+    # top attacking IPs
+    ip_counter = Counter()
+    for alert in alerts:
+        ip = extract_ip(alert)
+        ip_counter[ip   ] +=1
+    
+    print("\nTop attacking IPs")
+    for ip, count in ip_counter.most_common():
+        print(f"{ip} - {count} alerts")
+
+    #Risk score by ip
+    risk_score = Counter()
+    for alert in alerts:
+        ip = extract_ip(alert)
+        text = alert.lower()
+
+        if "flood" in text:
+            risk_score[ip] += RISK_WEIGHTS['flood']
+
+        elif "sql injection" in text:
+            risk_score[ip] += RISK_WEIGHTS['sqli']
+
+        elif "brute force" in text:
+            risk_score[ip] += RISK_WEIGHTS['brute']
+
+        elif "directory enumeration" in text:
+            risk_score[ip] += RISK_WEIGHTS['scan']
+
+        elif "sensitive path" in text:
+            risk_score[ip] += RISK_WEIGHTS['sensitive']
+
+        elif "user-agent" in text:
+            risk_score[ip] += RISK_WEIGHTS['user-agent']
+            
+    print("\nRisk score by ip")
+    for ip, score in risk_score.most_common():
+        if score>=10:
+            level = "HIGH"
+        elif score >=5:
+            level = "MEDIUM"
+        else:
+            level = "LOW"
+        print(f"{ip} - {level} ({score})")
+             
